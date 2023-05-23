977ff691d5f03ae15fe44f48bb75420f0cc6851d
KyRtAp-alt <patrykwronka65@wp.pl>
2023-05-23 13:27:51 +0200

    naprawione

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,22 +1,12 @@
-import {
-  StatusBar
-} from 'expo-status-bar';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return ( <
-    View style = {
-      styles.container
-    } >
-    <
-    Text > Co to za zagadka dwie kulki i armatka < /Text> <
-    StatusBar style = "auto" / >
-    <
-    /View>
+  return (
+    <View style={styles.container}>
+      <Text>Co to za zagadka dwie kulki i armatka</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
