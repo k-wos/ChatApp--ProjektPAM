85e6e0ac168d3e083807a07a4b84aafb70472e29
KyRtAp-alt <patrykwronka65@wp.pl>
2023-05-23 13:24:49 +0200

    com1

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StatusBar
+} from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Co to za zagadka dwie kulki i </Text>
-      <StatusBar style="auto" />
-    </View>
+  return ( <
+    View style = {
+      styles.container
+    } >
+    <
+    Text > Co to za zagadka dwie kulki i armatka < /Text> <
+    StatusBar style = "auto" / >
+    <
+    /View>
   );
 }
 
