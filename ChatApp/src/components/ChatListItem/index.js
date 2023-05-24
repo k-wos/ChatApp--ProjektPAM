98a3f954136469baf7c562fb9a2aa619a9c5f6f7
Dezmerek <mszymanski027@gmail.com>
2023-05-24 10:11:59 +0200

    render flatlist

--- ChatApp/src/components/ChatListItem/index.js
+++ ChatApp/src/components/ChatListItem/index.js
@@ -3,23 +3,23 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
-const ChatListItem = ({chat}) => {
+const ChatListItem = ({ chat }) => {
   return (
     <View style={styles.container}>
-     <Image source={{ uri: chat.user.image }} style={styles.image} />
-      
+      <Image source={{ uri: chat.user.image }} style={styles.image} />
+
       <View style={styles.content}>
-		<View style={styles.row}>
-	        <Text num style={styles.name}>
+        <View style={styles.row}>
+          <Text num style={styles.name}>
             {chat.user.name}
-            </Text>
-		    <Text style={styles.subTitle}>
-        {dayjs(chat.lastMessage.createdAt).fromNow()}
-            </Text>
-	      </View>
+          </Text>
+          <Text style={styles.subTitle}>
+            {dayjs(chat.lastMessage.createdAt).fromNow()}
+          </Text>
+        </View>
 
         <Text numberOfLines={2} style={styles.subTitle}>
-        {chat.lastMessage.text}
+          {chat.lastMessage.text}
         </Text>
       </View>
     </View>
@@ -27,35 +27,35 @@ const ChatListItem = ({chat}) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-      marginHorizontal: 10,
-      marginVertical: 5,
-      height: 70,
-    },
-    image: {
-      width: 60,
-      height: 60,
-      borderRadius: 30,
-      marginRight: 10,
-    },
-    content: {
-      flex: 1,
-  
-      borderBottomWidth: StyleSheet.hairlineWidth,
-      borderBottomColor: 'lightgray',
-    },
-    row: {
-      flexDirection: 'row',
-      marginBottom: 5,
-    },
-    name: {
-      flex: 1,
-      fontWeight: 'bold',
-    },
-    subTitle: {
-      color: 'gray',
-    },
-  });
+  container: {
+    flexDirection: 'row',
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  content: {
+    flex: 1,
+
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'lightgray',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  name: {
+    flex: 1,
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    color: 'gray',
+  },
+});
 
 export default ChatListItem;
