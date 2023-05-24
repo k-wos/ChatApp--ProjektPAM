909cca496a01efea8d896cd616c61bbe0229d17e
Dezmerek <mszymanski027@gmail.com>
2023-05-24 10:04:53 +0200

    create messgae component

---
+++ ChatApp/src/components/Message/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Message = ({ message }) => {
+  return (
+    <View>
+      <Text>{message.text}</Text>
+    </View>
+  );
+};
+
+export default Message;
