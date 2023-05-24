f366961fd8aac985b09fd52a2f8d4f158d814e13
Dezmerek <mszymanski027@gmail.com>
2023-05-25 01:27:10 +0200

    auth

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Navigator from "./src/navigation";
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+import { withAuthenticator } from "aws-amplify-react-native";
 
-export default function App() {
+Amplify.configure(awsconfig)
+
+function App() {
   return (
     <View style={styles.container}>
       <Navigator />
@@ -19,3 +24,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default withAuthenticator(App);
