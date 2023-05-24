578e3f6ef810b9104141e54f5907dee64df871e4
Dezmerek <mszymanski027@gmail.com>
2023-05-24 11:01:27 +0200

    stack navigator

--- ChatApp/src/navigation/index.js
+++ ChatApp/src/navigation/index.js
@@ -1,10 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import ChatsScreen from "../screens/ChatsScreen";
+import ChatScreen from "../screens/ChatScreen";
+
+const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
     return (
         <NavigationContainer>
-            <ChatsScreen />
+            <Stack.Navigator>
+                <Stack.Screen name="Chats" component={ChatsScreen} />
+                <Stack.Screen name="Chat" component={ChatScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 };
