9c0083185613ceae132329f93e05ae6b441ab821
Dezmerek <mszymanski027@gmail.com>
2023-05-24 10:31:24 +0200

    create component inputbox

---
+++ ChatApp/src/components/InputBox/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const InputBox = () => {
+    return (
+        <View>
+            <Text>InputBox</Text>
+        </View>
+    )
+}
+
+export default InputBox
