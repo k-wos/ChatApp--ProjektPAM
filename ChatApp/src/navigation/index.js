524f9afc0aa350ac42c1d3e1588d70639cc4dcca
Dezmerek <mszymanski027@gmail.com>
2023-05-24 13:15:33 +0200

    edit .js

--- ChatApp/src/navigation/index.js
+++ ChatApp/src/navigation/index.js
@@ -1,9 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import ChatsScreen from "../screens/ChatsScreen";
 import ChatScreen from "../screens/ChatScreen";
-import MainTabNavigator from "./MainTabNavigator.js";
+import ContactsScreen from "../screens/ContactsScreen";
+import MainTabNavigator from "./MainTabNavigator";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +13,7 @@ const Navigator = () => {
             <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: 'whitesmoke' } }}>
                 <Stack.Screen name="Main" component={MainTabNavigator} options={{ headerShown: false }} />
                 <Stack.Screen name="Chat" component={ChatScreen} />
+                <Stack.Screen name="Contacts" component={ContactsScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
