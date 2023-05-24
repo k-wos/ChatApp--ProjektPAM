1dbb9ce2bbb747a3066b15ffb2566cca74999dcd
Dezmerek <mszymanski027@gmail.com>
2023-05-24 12:41:19 +0200

    main tab navigator end

--- ChatApp/src/navigation/index.js
+++ ChatApp/src/navigation/index.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import ChatsScreen from "../screens/ChatsScreen";
 import ChatScreen from "../screens/ChatScreen";
+import MainTabNavigator from "./MainTabNavigator.js";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +11,7 @@ const Navigator = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Chats" component={ChatsScreen} />
+                <Stack.Screen name="Main" component={MainTabNavigator} options={{ headerShown: false }} />
                 <Stack.Screen name="Chat" component={ChatScreen} />
             </Stack.Navigator>
         </NavigationContainer>
