38aa279b96bdda2da3cf6bb1475a95f105609cef
Dezmerek <mszymanski027@gmail.com>
2023-05-24 13:13:02 +0200

    add ContactsScreen

---
+++ ChatApp/src/screens/ContactsScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const ContactsScreen = () => {
+    return (
+        <View>
+            <Text>ContactsScreen</Text>
+        </View>
+    );
+};
+
+export default ContactsScreen;
