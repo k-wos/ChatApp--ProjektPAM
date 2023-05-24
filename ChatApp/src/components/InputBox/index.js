1f09a7dca114f3dba259aeb70381f7d1b493dbab
Dezmerek <mszymanski027@gmail.com>
2023-05-24 10:51:15 +0200

    manage user input

--- ChatApp/src/components/InputBox/index.js
+++ ChatApp/src/components/InputBox/index.js
@@ -1,42 +1,56 @@
-import { View, TextInput, StyleSheet } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
+import { useState } from 'react';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { AntDesign, MaterialIcons } from '@expo/vector-icons';
 
 const InputBox = () => {
+    const [newMessage, setNewMessage] = useState('');
+
+    const onSend = () => {
+        console.warn('Sending a new message: ', newMessage);
+
+        setNewMessage('');
+    };
+
     return (
         <View style={styles.container}>
-            <AntDesign name="plus" size={24} color="royalblue" />
-            <TextInput style={styles.input} placeholder="type your message..." />
-            <MaterialIcons style={styles.send} name="send" size={24} color="white" />
+            <AntDesign name="plus" size={20} color="royalblue" />
+
+            <TextInput
+                value={newMessage}
+                onChangeText={setNewMessage}
+                style={styles.input}
+                placeholder="type your message..."
+            />
+
+            <MaterialIcons onPress={onSend} style={styles.send} name="send" size={16} color="white" />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: "row",
-        backgroundColor: "whitesmoke",
+        flexDirection: 'row',
+        backgroundColor: 'whitesmoke',
         padding: 5,
-        alignItems: "center",
+        paddingHorizontal: 10,
+        alignItems: 'center',
     },
     input: {
-        fontSize: 18,
-
         flex: 1,
-        backgroundColor: "white",
+        backgroundColor: 'white',
         padding: 5,
         paddingHorizontal: 10,
         marginHorizontal: 10,
 
         borderRadius: 50,
+        borderColor: 'lightgray',
         borderWidth: StyleSheet.hairlineWidth,
-        borderColor: "lightgray",
     },
     send: {
-        backgroundColor: "royalblue",
+        backgroundColor: 'royalblue',
         padding: 7,
         borderRadius: 15,
-        overflow: "hidden",
+        overflow: 'hidden',
     },
 });
 
