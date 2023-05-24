37668b845164861dddfccbbe5c0300f57aa1e1af
Dezmerek <mszymanski027@gmail.com>
2023-05-24 12:48:10 +0200

    add icon

--- ChatApp/src/navigation/MainTabNavigator.js.js
+++ ChatApp/src/navigation/MainTabNavigator.js.js
@@ -1,17 +1,18 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ChatsScreen from "../screens/ChatsScreen";
 import NotImplementedScreen from "../screens/NotImplementedScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const MainTabNavigator = () => {
     return (
         <Tab.Navigator initialRouteName="Chats">
-            <Tab.Screen name="Status" component={NotImplementedScreen} />
-            <Tab.Screen name="Calls" component={NotImplementedScreen} />
-            <Tab.Screen name="Camera" component={NotImplementedScreen} />
-            <Tab.Screen name="Chats" component={ChatsScreen} />
-            <Tab.Screen name="Settings" component={NotImplementedScreen} />
+            <Tab.Screen name="Status" component={NotImplementedScreen} options={{ tabBarIcon: ({ color, size }) => (<Ionicons name="logo-whatsapp" size={size} color={color} />), }} />
+            <Tab.Screen name="Calls" component={NotImplementedScreen} options={{ tabBarIcon: ({ color, size }) => (<Ionicons name="call-outline" size={size} color={color} />), }} />
+            <Tab.Screen name="Camera" component={NotImplementedScreen} options={{ tabBarIcon: ({ color, size }) => (<Ionicons name="camera-outline" size={size} color={color} />), }} />
+            <Tab.Screen name="Chats" component={ChatsScreen} options={{ tabBarIcon: ({ color, size }) => (<Ionicons name="ios-chatbubbles-sharp" size={size} color={color} />), }} />
+            <Tab.Screen name="Settings" component={NotImplementedScreen} options={{ tabBarIcon: ({ color, size }) => (<Ionicons name="settings-outline" size={size} color={color} />), }} />
         </Tab.Navigator>
     );
 };
