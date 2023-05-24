11c1e5a2d01b5398a051ed61818aa5d199204960
Dezmerek <mszymanski027@gmail.com>
2023-05-24 13:05:52 +0200

    edit

--- ChatApp/src/screens/ChatScreen.js
+++ ChatApp/src/screens/ChatScreen.js
@@ -1,34 +1,33 @@
 import { useEffect } from 'react';
-import { Text, ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from "react-native";
-import { useRoute, useNavigation } from "@react-navigation/native";
+import { ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Message from '../components/Message';
+import InputBox from '../components/InputBox';
 
-import bg from "../../assets/images/BG.png";
-import Message from "../components/Message";
-import messages from "../../assets/data/messages.json";
-import InputBox from "../components/InputBox";
+import bg from '../../assets/images/BG.png';
+import messages from '../../assets/data/messages.json';
 
 const ChatScreen = () => {
-    const navigation = useNavigation();
     const route = useRoute();
+    const navigation = useNavigation();
 
     useEffect(() => {
         navigation.setOptions({ title: route.params.name });
-    }, [route.params]);
+    }, [route.params.name]);
 
     return (
         <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 90}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 90}
             style={styles.bg}
         >
             <ImageBackground source={bg} style={styles.bg}>
                 <FlatList
                     data={messages}
                     renderItem={({ item }) => <Message message={item} />}
-                    style={{ padding: 10 }}
+                    style={styles.list}
                     inverted
                 />
-
                 <InputBox />
             </ImageBackground>
         </KeyboardAvoidingView>
@@ -38,6 +37,9 @@ const ChatScreen = () => {
 const styles = StyleSheet.create({
     bg: {
         flex: 1,
+    },
+    list: {
+        padding: 10,
     },
 });
 
