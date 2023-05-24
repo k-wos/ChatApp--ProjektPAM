60f16b84c0a3b133f590a5a86dcfeafbc42b833a
Dezmerek <mszymanski027@gmail.com>
2023-05-24 10:37:59 +0200

    style inputbox

--- ChatApp/src/components/InputBox/index.js
+++ ChatApp/src/components/InputBox/index.js
@@ -6,16 +6,38 @@ const InputBox = () => {
     return (
         <View style={styles.container}>
             <AntDesign name="plus" size={24} color="royalblue" />
-            <TextInput style={styles.input} />
+            <TextInput style={styles.input} placeholder="type your message..." />
             <MaterialIcons style={styles.send} name="send" size={24} color="white" />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {},
-    input: {},
-    send: {},
+    container: {
+        flexDirection: "row",
+        backgroundColor: "whitesmoke",
+        padding: 5,
+        alignItems: "center",
+    },
+    input: {
+        fontSize: 18,
+
+        flex: 1,
+        backgroundColor: "white",
+        padding: 5,
+        paddingHorizontal: 10,
+        marginHorizontal: 10,
+
+        borderRadius: 50,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: "lightgray",
+    },
+    send: {
+        backgroundColor: "royalblue",
+        padding: 7,
+        borderRadius: 15,
+        overflow: "hidden",
+    },
 });
 
 export default InputBox;
