d78588898c51fe992b7b42656e63eb99007d5a6b
Dezmerek <mszymanski027@gmail.com>
2023-05-24 09:42:57 +0200

    edit chatscreen

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ChatsScreen from "./src/screens/ChatsScreen";
+import ChatScreen from "./src/screens/ChatScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatsScreen />
+      <ChatScreen />
 
       <StatusBar style="auto" />
     </View>
